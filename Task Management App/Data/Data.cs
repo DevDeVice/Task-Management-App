f183df34e1ad9b30814e8175ad707f3b828818d7
DevDeVice <40960987+DevDeVice@users.noreply.github.com>
2024-07-08 21:46:23 +0200

    Initiation, data and models

---
+++ Task Management App/Data/Data.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TaskManagementAPI.Models;
+
+namespace TaskManagementAPI.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
