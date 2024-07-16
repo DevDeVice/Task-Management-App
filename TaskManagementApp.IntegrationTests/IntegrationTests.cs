381fbcd03649be4d6dd630f5233a0d4a9c340e6e
DevDeVice <40960987+DevDeVice@users.noreply.github.com>
2024-07-16 17:19:56 +0200

    Tests folders

---
+++ TaskManagementApp.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TaskManagementApp;
+
+public class IntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+{
+    private readonly HttpClient _client;
+
+    public IntegrationTests(WebApplicationFactory<Startup> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async void GetTasks_ReturnsSuccessStatusCode()
+    {
+        // Act
+        var response = await _client.GetAsync("/api/tasks");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Task", responseString);
+    }
+}
---
+++ TaskManagementApp.UnitTests/TasksControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Moq;
+using TaskManagementApp.Controllers;
+using TaskManagementApp.Models;
+using TaskManagementApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class TasksControllerTests
+{
+    [Fact]
+    public async Task GetTasks_ReturnsOkResult_WithAListOfTaskItems()
+    {
+        // Arrange
+        var mockService = new Mock<ITaskService>();
+        mockService.Setup(service => service.GetTasks())
+                   .ReturnsAsync(new List<TaskItem> { new TaskItem { Id = 1, Title = "Test Task", Description = "Test Description", IsComplete = false } });
+
+        var controller = new TasksController(mockService.Object);
+
+        // Act
+        var result = await controller.GetTasks();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<TaskItem>>(okResult.Value);
+        Assert.Single(returnValue);
+    }
+}
